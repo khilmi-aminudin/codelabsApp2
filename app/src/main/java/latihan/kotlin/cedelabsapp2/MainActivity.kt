2266423435fe4b5b9e9ac6f89a5ceb2d3955094f
GMV <gmv@GMVs-MacBook-Pro.local>
2020-02-24 16:26:32 +0700

    Menambahkan File

---
+++ app/src/main/java/latihan/kotlin/cedelabsapp2/MainActivity.kt
@@ -0,0 +1,12 @@
+package latihan.kotlin.cedelabsapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
